a7cfbf7649f493117443ec21c50258e5b66380d8
Fajri Siiv <fajrijijin@gmail.com>
2022-11-15 20:15:25 +0700

    update error

--- components/Review/index.tsx
+++ components/Review/index.tsx
@@ -4,8 +4,9 @@ export default function Review() {
   return (
     <div className="bg-[#0B015B] min-h-[500px] text-white p-20">
       <h3 className="text-[4.4rem] font-bold mb-10 leading-[5.5rem]">
-        "I really enjoyed working with you guys, you guys are very communicative
-        and quick to do the job. I really can't wait to work with you again!"
+        &quot;I really enjoyed working with you guys, you guys are very
+        communicative and quick to do the job. I really cant wait to work with
+        you again!&quot;
       </h3>
       <span className="text-xl font-semibold tracking-widest">
         Muhammad Fajri
